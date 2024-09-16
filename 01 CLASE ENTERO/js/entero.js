5aa2a4eacf00807a05d46e454371e3fc55c19354
ApuFernan <luis.palacios.c@ucb.edu.bo>
2024-09-16 12:23:48 -0400

    intento html

--- 01 CLASE ENTERO/js/entero.js
+++ 01 CLASE ENTERO/js/entero.js
@@ -1,110 +1,74 @@
-class Entero
-{
-    //atributos
+class Entero {
+    //Atributos
     Num;
-
-    //constructor - inicializa el objeto
-    constructor(Numero)
-    {
+    //Constructor = inicializa el objeto
+    constructor(Numero){
         this.Num = Numero;
     }
-
-    //cargar el valor de Num
-    setNum()
-    {
-        const input = document.getElementById("numeroInput");
+    //CLASS - Cargar el valor de Num
+    setNum(){
+        const input = document.getElementById('numeroInput');
+         
         this.Num = parseInt(input.value, 10);
     }
-
     //CLASS - Obtener el valor de Num
-    getNum()
-    {
+    getNum() {
         return this.Num;
     }
-
     //CLASS - Mostrar el valor de Num
-    showNum()
-    {
-        const resultadoDiv = document.getElementById("resultado");
+    showNum(){
+        const resultadoDiv = document.getElementById('resultado');
         resultadoDiv.textContent = this.getNum();
     }
 
-    showResultado(respuesta)
-    {
-        const resultadoDiv = document.getElementById("resultado");
+    showResultado(respuesta){
+        const resultadoDiv = document.getElementById('resultado');
         resultadoDiv.textContent = respuesta;
     }
 
-    showParImpar(respuesta_parimpar)
-    {
-        const resultadoDiv = document.getElementById("resultado");
-        resultadoDiv.textContent = respuesta_parimpar;
-    }
-
-    incrementarNum()
-    {
+    incrementarNum(){
         this.Num = this.Num + 1;
     }
 
-    decrementarNum()
-    {
+    decrementarNum(){
         this.Num = this.Num - 1;
     }
-
-    esParImpar()
-    {
+    
+    esParImpar(){
         return (this.Num % 2 == 0)
     }
 
-    esPositivoNegativo()
+    numPerfecto()
     {
-        return (this.Num > 0)
+        
     }
+
 }
 
-//las funciones = button HTML
-var ClaseEntero = new Entero(0); //se inicializo en 0
+//Las funciones = button HTML
+var ClaseEntero = new Entero(0); //Se inicializo en 0
 
-//crear el objeto
-function cargarNum()
-{
+//Crear el objeto
+function cargarNum() {
     ClaseEntero.setNum();
 }
-
-//mostrar el valor de hum en html
-function mostrarNum()
-{
+//Mostrar el valor de Num en HTML
+function mostrarNum(){
     ClaseEntero.showNum();
 }
-
-//incrementar el valor de NUM
-function incrementarValor()
-{
+//Incrementar el valor de Num
+function incrementarValor(){
     ClaseEntero.incrementarNum();
     ClaseEntero.showNum();
 }
-
-//decrementar el valor de NUM
-function decrementarValor()
-{
+//Decrementar el valor de Num
+function decrementarValor(){
     ClaseEntero.decrementarNum();
     ClaseEntero.showNum();
 }
-
-//verifica si es par o impar
-function esParImparNum()
-{
+//Verifica si es Par o Impar
+function esParImparNum(){
     var respuesta = ClaseEntero.esParImpar();
     var resp = respuesta ? "Es Num Par":"Es Num Impar";
     ClaseEntero.showResultado(resp);
-
-}
-
-//verifica si es negativo o positivo
-function esPositivoNegativoNum()
-{
-    var respuesta_parimpar = ClaseEntero.esPositivoNegativo();
-    var verif_parimpar = respuesta_parimpar ? "Es Positivo":"Es Negativo";
-    ClaseEntero.showParImpar(verif_parimpar);
-
 }
