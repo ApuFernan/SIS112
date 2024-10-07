b7284f26e34f1f849e70fe4f5405ad5b22e61da5
ApuFernan <luis.palacios.c@ucb.edu.bo>
2024-10-07 11:36:38 -0400

    Listas

--- 03 CLASE LISTA DE ENTEROS/js/lista-cadenas.js
+++ 03 CLASE LISTA DE ENTEROS/js/lista-cadenas.js
@@ -5,52 +5,99 @@ class ListaCadenas {
 
     agregar(valor) {
         this.lista.push(valor);
+        this.mostrarLista();
     }
 
     eliminar(valor) {
-        const index =this.lista.indexOf(valor);
+        const index = this.lista.indexOf(valor);
         if (index !== -1) {
-            this.lista.splice(index,1);
+            this.lista.splice(index, 1);
         }
+        this.mostrarLista();
     }
 
     buscar(valor) {
         return this.lista.indexOf(valor);
-    }  
+    }
 
     ordenar() {
-        this.lista.sort((a,b) => a - b); //orden ascendente
+        this.lista.sort((a, b) => a - b); // Orden ascendente
+        this.mostrarLista();
+    }
+
+    mostrarLista() {
+        const listaElement = document.getElementById('listaCadenas');
+        listaElement.innerHTML = '';
+        this.lista.forEach(cadena => {
+            const li = document.createElement('li');
+            li.textContent = cadena;
+            listaElement.appendChild(li);
+        });
     }
 }
 
-const listaEnteros = new ListaEnteros();
-const input = document.getElementById('numeroInput');
-const agregarBtn = document.getElementById('agregarBtn');
-const listaNumeros = document.getElementById('listaNumeros');
-const eliminarBtn = document.getElementById('EliminarBtn');
-const BuscarUnDato = document.getElementById('BuscarlosDatos');
-const OrdenarDato = document.getElementById('OrdenarDato');
+const listaCadenas = new ListaCadenas();
+listaCadenas.agregar('Hola');
+listaCadenas.agregar('Mundo');
+listaCadenas.agregar('UCB');
 
-agregarBtn.addEventListener('click', () => {
-    const valor = parseInt(input.value);
-    if (!isNaN(valor)) {
-        listaEnteros.agregar(valor);
-        mostrarLista();
-        input.value = '';
+//Retornar la cadena con mayor caracteres
+
+//var = let
+//------------------------------------Hola
+var a = listaCadenas.lista[0]
+console.log(a);
+var aCant = a.length;
+console.log(aCant);
+//------------------------------------Mundo
+var b = listaCadenas.lista[1]
+console.log(b);
+var bCant = b.length
+console.log(bCant);
+//------------------------------------UCB
+var c = listaCadenas.lista[2]
+console.log(c);
+var cCant = c.length
+console.log(cCant);
+
+
+console.log(listaCadenas.lista); // Imprime: ['Hola', 'Mundo', 'UCB']
+
+
+
+
+
+
+
+
+
+
+document.getElementById('btnAgregar').onclick = () => {
+    const valor = document.getElementById('inputValor').value.trim();
+    if (valor) {
+        listaCadenas.agregar(valor);
+        document.getElementById('inputValor').value = '';
+    }
+};
+
+document.getElementById('btnEliminar').onclick = () => {
+    const valor = document.getElementById('inputEliminar').value.trim();
+    if (valor) {
+        listaCadenas.eliminar(valor);
+        document.getElementById('inputEliminar').value = '';
+    }
+};
+
+document.getElementById('btnBuscar').onclick = () => {
+    const valor = document.getElementById('inputBuscar').value.trim();
+    const index = listaCadenas.buscar(valor);
+    if (index !== -1) {
+        alert(`Cadena encontrada en el índice: ${index}`);
     } else {
-        alert('Por favor, ingresa un número válido.');
+        alert('Cadena no encontrada.');
     }
-});
+};
 
-function mostrarLista() {
-    listaNumeros.innerHTML = '';
-    listaEnteros.lista.forEach(num => {
-        const li = document.createElement('li');
-        li.textContent = num;
-        listaNumeros.appendChild(li);
-    });
-}
-let miLista = new ListaCadenas();
-miLista.agregar("Hola");
-miLista.agregar("Mundo");
-console.log(miLista.lista); // Imprime: ["Hola", "Mundo"]
+document.getElementById('btnOrdenar').onclick = () => {
+    listaCadenas.ordenar();
+};
--- 03 CLASE LISTA DE ENTEROS/js/lista-enteros.js
+++ 03 CLASE LISTA DE ENTEROS/js/lista-enteros.js
@@ -8,45 +8,158 @@ class ListaEnteros {
     }
 
     eliminar(valor) {
-        const index =this.lista.indexOf(valor);
+        const index = this.lista.indexOf(valor);
         if (index !== -1) {
-            this.lista.splice(index,1);
+            this.lista.splice(index, 1);
         }
     }
 
     buscar(valor) {
         return this.lista.indexOf(valor);
-    }  
+    }
 
     ordenar() {
-        this.lista.sort((a,b) => a - b); //orden ascendente
+        this.lista.sort((a, b) => a - b); // Orden ascendente
+    }
+
+    mostrarLista() {
+        const listaResultado = document.getElementById('listaResultado');
+        listaResultado.innerHTML = '';
+        this.lista.forEach(valor => {
+            const li = document.createElement('li');
+            li.textContent = valor;
+            listaResultado.appendChild(li);
+        });
+        this.mostrarMensaje('Lista actualizada.');
+    }
+
+    mostrarMensaje(mensaje) {
+        const divMensaje = document.getElementById('mensaje');
+        divMensaje.textContent = mensaje;
+        setTimeout(() => {
+            divMensaje.textContent = '';
+        }, 3000);
     }
 }
 
 const listaEnteros = new ListaEnteros();
-const input = document.getElementById('numeroInput');
-const agregarBtn = document.getElementById('agregarBtn');
-const listaNumeros = document.getElementById('listaNumeros');
-const eliminarBtn = document.getElementById('EliminarBtn');
-const BuscarUnDato = document.getElementById('BuscarlosDatos');
-const OrdenarDato = document.getElementById('OrdenarDato');
+listaEnteros.agregar(21);
+listaEnteros.agregar(12);
+listaEnteros.agregar(15);
+listaEnteros.agregar(30);
+console.log(listaEnteros.lista); // Imprime: [21, 12, 15, 30]
 
-agregarBtn.addEventListener('click', () => {
-    const valor = parseInt(input.value);
+var a = listaEnteros.lista[0];
+console.log(a);
+
+var b = listaEnteros.lista[1];
+console.log(b);
+
+var c = listaEnteros.lista[2];
+console.log(c);
+
+var d = listaEnteros.lista[3];
+console.log(d);
+
+//RETORNAR EL ELEMENTO MENOR DE LA LISTA, dado  que la lista solo almacena valores positivos
+/*
+var datoMenor = -1;
+//------------------------------------------------
+var a = listaEnteros.lista[0];
+if(a > datoMenor){
+    datoMenor = a;
+}
+console.log("Dato Menor: " + datoMenor);
+//------------------------------------------------
+var b = listaEnteros.lista[1];
+if(a > b){
+    datoMenor = b;
+}
+console.log("Dato Menor: " + datoMenor);
+//------------------------------------------------
+var c = listaEnteros.lista[2];
+if(b > c){
+    datoMenor = c;
+}
+console.log("Dato Menor: " + datoMenor);
+//------------------------------------------------
+var d = listaEnteros.lista[3];
+if(c > d){
+    datoMenor = d;
+}
+console.log("Dato Menor: " + datoMenor);
+//------------------------------------------------
+*/
+
+var datoMenor=listaEnteros.lista[0];
+for (let index = 1; index < listaEnteros.lista.length; index++) {
+    if(listaEnteros.lista[index]< datoMenor){
+        datoMenor=listaEnteros.lista[index]
+    }
+    console.log("Dato Menor: " + datoMenor);
+}
+
+
+const lista = listaEnteros.lista; // Acceder a la lista una sola vez
+
+if (lista.length === 0) {
+    console.log("La lista está vacía.");
+} else {
+    let datoMayor = lista[0]; // Inicializar el dato mayor
+
+    for (const num of lista) { // Usar un bucle for-of para mayor claridad
+        if (num > datoMayor) {
+            datoMayor = num; // Actualizar el dato mayor
+        }
+    }
+
+    console.log("Dato Mayor:", datoMayor);
+}
+
+
+
+
+
+
+
+
+
+
+document.getElementById('btnAgregar').addEventListener('click', () => {
+    const valor = parseInt(document.getElementById('inputValor').value);
     if (!isNaN(valor)) {
         listaEnteros.agregar(valor);
-        mostrarLista();
-        input.value = '';
+        document.getElementById('inputValor').value = '';
     } else {
-        alert('Por favor, ingresa un número válido.');
+        listaEnteros.mostrarMensaje('Por favor, ingrese un número válido.');
     }
 });
 
-function mostrarLista() {
-    listaNumeros.innerHTML = '';
-    listaEnteros.lista.forEach(num => {
-        const li = document.createElement('li');
-        li.textContent = num;
-        listaNumeros.appendChild(li);
-    });
-}
+document.getElementById('btnEliminar').addEventListener('click', () => {
+    const valor = parseInt(document.getElementById('inputValor').value);
+    if (!isNaN(valor)) {
+        listaEnteros.eliminar(valor);
+        document.getElementById('inputValor').value = '';
+    } else {
+        listaEnteros.mostrarMensaje('Por favor, ingrese un número válido.');
+    }
+});
+
+document.getElementById('btnBuscar').addEventListener('click', () => {
+    const valor = parseInt(document.getElementById('inputValor').value);
+    if (!isNaN(valor)) {
+        const index = listaEnteros.buscar(valor);
+        if (index !== -1) {
+            listaEnteros.mostrarMensaje(`El número ${valor} se encuentra en la lista.`);
+        } else {
+            listaEnteros.mostrarMensaje(`El número ${valor} no se encuentra en la lista.`);
+        }
+        document.getElementById('inputValor').value = '';
+    } else {
+        listaEnteros.mostrarMensaje('Por favor, ingrese un número válido.');
+    }
+});
+
+document.getElementById('btnOrdenar').addEventListener('click', () => {
+    listaEnteros.ordenar();
+});
